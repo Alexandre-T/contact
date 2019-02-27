1d0f28363e146b5238567484ee3ff065a9e0e6d3
Alexandre-T <alexandre.tranchant@gmail.com>
2019-02-27 12:48:33 +0100

    Organization edit form optimized.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,8 @@ require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
+Window.prototype.$ = $;
+
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
@@ -18,9 +20,10 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
 // or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
+require('bootstrap/js/dist/tooltip');
+require('bootstrap/js/dist/popover');
 
 $(document).ready(function() {
   $('[data-toggle="popover"]').popover();
+  $('[data-toggle="tooltip"]').tooltip();
 });
