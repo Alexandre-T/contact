2df21f309136562b4c9e50821d36134979144dfb
Alexandre-T <alexandre.tranchant@gmail.com>
2019-03-13 21:45:39 +0100

    Functional test initialized.

---
+++ tests/functional/HomeCest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the contact Application.
+ *
+ * PHP version 7.2
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * @category Functional test
+ *
+ * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @copyright 2019 Cerema
+ * @license   CeCILL-B V1
+ *
+ * @see       http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+ */
+
+
+namespace App\Tests\functional;
+
+
+use App\Tests\FunctionalTester;
+
+class HomeCest
+{
+    public function index(FunctionalTester $I)
+    {
+        $I->amOnPage('/');
+        $I->canSeeResponseCodeIsSuccessful();
+    }
+}
